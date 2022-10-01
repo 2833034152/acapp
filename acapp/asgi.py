2b3942245674ca85956a8c57f2527440e73a7a6b
2833034152 <2833034152@qq.com>
2022-10-01 23:42:48 +0800

    implement thrift service

--- acapp/asgi.py
+++ acapp/asgi.py
@@ -6,17 +6,25 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
+
 import os
+
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acapp.settings')
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from game.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acapp.settings')
+from channels.layers import get_channel_layer
+channel_layer = get_channel_layer()
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
 })
+
 
